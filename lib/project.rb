5299e4f3479476d650a3e6d4e5a9c75b6881bdf9
Marcus Jensen <marcusjensen1515@gmail.com>
2019-12-13 10:05:24 -0800

    both capitalize reader methods now functional

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,16 @@
 class Anagrams
   def initialize(input1, input2)
-    @input1 = input1
-    @input2 = input2
+    @input1 = input1.upcase
+    @input2 = input2.upcase
     @input1_length = input1.length
     @input2_length = input2.length
   end #ends initialize
+  def input1_check
+    @input1
+  end #ends input 1 method
+  def input2_check
+    @input2
+  end #ends input2 method
   def length_check
     if @input1_length != @input2_length
       "These words are not Anagrams"
@@ -13,3 +19,10 @@ class Anagrams
     end #ends length_check if statement
   end #ends length_check
 end #ends Anagrams class
+
+
+
+
+
+
+#you can also run the sort method and if those are equal it basically does the whole project for you
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,11 +1,29 @@
 require('rspec')
 require('project')
 
-describe ('#project') do
+describe ('#project length test') do
   it("should check to see if the lengths of both inputs are equal")do
-    anagrams = Anagrams.new("hello","helloooo")
+    anagrams = Anagrams.new("hello","hello")
 
-    expect(anagrams.length_check).to(eq("These words are not Anagrams"))
+    expect(anagrams.length_check).to(eq("Run vowel check function"))
+
+  end
+end
+
+describe ('#project input1 captitalize') do
+  it("should capitalize every letter for input one")do
+    anagrams = Anagrams.new("hello","hello")
+
+    expect(anagrams.input1_check).to(eq("HELLO"))
+
+  end
+end
+
+describe ('#project input2 captitalize') do
+  it("should capitalize every letter for input two")do
+    anagrams = Anagrams.new("hello","hello")
+
+    expect(anagrams.input2_check).to(eq("HELLO"))
 
   end
 end
